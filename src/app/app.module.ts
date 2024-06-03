985dd5bf22d762b3fe8d8e73b973eac96890614f
SergioMelado <sergio.melado@soprasteria.com>
2024-06-03 16:30:33 +0200

    navigation and dashboard created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,21 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
     MessagesComponent,
     HeroDetailComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    FormsModule
-    
-  ],
-  providers: [
-    // no need to place any providers due to the `providedIn` flag...
+    FormsModule,
   ],
   bootstrap: [ AppComponent ]
 })
