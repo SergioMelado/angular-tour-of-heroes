5f012c9e9ef400f599569e2f7b13c78e5eeb1428
SergioMelado <sergio.melado@soprasteria.com>
2024-06-05 11:00:21 +0200

    Fixed update and delete problems

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -90,7 +90,7 @@ export class HeroService {
 
   /** PUT: update the hero on the server */
   updateHero(hero: Hero): Observable<any> {
-    return this.http.put(this.heroesUrlApiRest, hero, this.httpOptions).pipe(
+    return this.http.patch(`${this.heroesUrlApiRest}/${hero.id}`, hero, this.httpOptions).pipe(
       tap(_ => this.log(`updated hero id=${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
     );
